7ac3551d490f95d7bb2f47dd6b0a5e86ac058d88
mizuki-boc <mizuki0842boc@icloud.com>
2021-04-04 19:59:22 +0900

    make UseApi class

--- discord_bot.py
+++ discord_bot.py
@@ -17,13 +17,16 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    use_api = trn_api.UseApi(user_name='m1zuki_the_predator')
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
     if message.content == '/hello':
         await message.channel.send('こんにちは！')
     if message.content == '/rp':
-        await message.channel.send(trn_api.get_rp(user_name='m1zThePredator'))
+        await message.channel.send(use_api.get_rp())
+    if message.content == '/rank':
+        await message.channel.send(use_api.get_rank())
 
 # Botの起動とDiscordサーバーへの接続
 client.run(os.environ['DISCORD_TOKEN'])
--- trn_api.py
+++ trn_api.py
@@ -3,22 +3,44 @@ import requests
 import json
 import os
 
-def get_rp(user_name):
-    base_url = 'https://public-api.tracker.gg/v2/apex/standard/'
-    params = {"TRN-Api-Key": os.environ['TRN_API_KEY']}
-    endpoint = "profile/origin/" + user_name
+# def get_rp(user_name):
+#     base_url = 'https://public-api.tracker.gg/v2/apex/standard/'
+#     params = {"TRN-Api-Key": os.environ['TRN_API_KEY']}
+#     endpoint = "profile/origin/" + user_name
 
-    session = requests.Session()
-    req = session.get(url=base_url+endpoint, params=params)
+#     session = requests.Session()
+#     req = session.get(url=base_url+endpoint, params=params)
 
-    req.close()
-    res = json.loads(req.text)
+#     req.close()
+#     res = json.loads(req.text)
 
-    rank_value = res["data"]["segments"][0]["stats"]["rankScore"]["value"]
-    rank_name = res["data"]["segments"][0]["stats"]["rankScore"]["metadata"]["rankName"]
-    print("ユーザ名: ", user_name)
-    print("ランク: " + rank_name + " (", rank_value,  ")")
+#     rank_value = res["data"]["segments"][0]["stats"]["rankScore"]["value"]
+#     rank_name = res["data"]["segments"][0]["stats"]["rankScore"]["metadata"]["rankName"]
+#     print("ユーザ名: ", user_name)
+#     print("ランク: " + rank_name + " (", rank_value,  ")")
 
-    ans = "ランク: " + rank_name + " (", rank_value,  ")"
+#     ans = "ランク: " + rank_name + " (", rank_value,  ")"
 
-    return rank_name
+#     return rank_name
+
+class UseApi:
+    def __init__(self, user_name):
+        self.user_name = user_name
+        # ここでApiたたく
+        base_url = 'https://public-api.tracker.gg/v2/apex/standard/'
+        params = {"TRN-Api-Key": os.environ['TRN_API_KEY']}
+        endpoint = "profile/origin/" + self.user_name
+
+        session = requests.Session()
+        req = session.get(url=base_url+endpoint, params=params)
+
+        req.close()
+        self.res = json.loads(req.text)
+    
+    def get_rp(self):
+        self.rp = self.res["data"]["segments"][0]["stats"]["rankScore"]["value"]
+        return self.rp
+
+    def get_rank(self):
+        self.rank = self.res["data"]["segments"][0]["stats"]["rankScore"]["metadata"]["rankName"]
+        return self.rank
